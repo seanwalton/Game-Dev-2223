4c145a9970444978b8c89e8cfdb7e68c3386b5a2
Sean Walton <sean@pillbug.zone>
2023-02-02 09:48:52 +0000

    The triagngle moves

---
+++ Assets/Scripts/TriangleBehaviour.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriangleBehaviour : MonoBehaviour
+{
+
+    [SerializeField] private float speed;
+
+    private Transform tr;
+    private Vector3 pos = new Vector3();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pos = tr.position;
+        pos.x += speed * Time.deltaTime;
+        tr.position = pos;
+    }
+}
